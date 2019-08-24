972aa3fbf2d8da06cf76f9e0bf7b48916c145808
Jetender <g.jetender@gmail.com>
2019-08-23 22:23:12 -0400

    trying things out

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,20 +1,8 @@
+
 //When Document Loads Up
-$(document).ready( function () {
+$(document).ready(function (){
 
-check =>{
-
-    let windowSize = $(window).width();
-
-switch (windowSize){
-
-    case 0:
-        
-        break;
-
-
-
-}
-}
-
+    
+    
 
 })
