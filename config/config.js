37069b887f6de8665e4e706873f778333dc5e792
Jetender <g.jetender@gmail.com>
2019-11-24 03:32:02 -0500

    heroku stiuff

--- config/config.js
+++ config/config.js
@@ -3,12 +3,8 @@ require('dotenv').config();
 module.exports =
   {
     "development": {
-      "username": "root",
-      "password": "root",
-      "database": "noteTaker_db",
-      "host": "localhost",
-      "dialect": "mysql",
-      "port": "3306",
+     "use_env_variable" :JAWSDB_URL,
+      "port": process.env.PORT,
       "logging": false,
       "storage": "./session.mysql"
     },
